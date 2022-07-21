b52fa29b12e150dca41ccaa401790a7d441341fb
malieskimi <malieskimi@yahoo.com>
2022-07-21 16:29:15 +0200

    Add nueget package props

--- test/Certificates/CertificateTest.cs
+++ test/Certificates/CertificateTest.cs
@@ -1,7 +1,7 @@
 namespace Datati.Libraries.Certificates.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class CertificateTest
     {
         [TestMethod]
         public void TestMethod1()
